f25d8e6cd8ac9fc6fd75cdcb2e95838038f325b5
Kazu Yamamoto <kazu@iij.ad.jp>
2011-12-20 12:01:49 +0900

    using TH.

--- test/Test.hs
+++ test/Test.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleInstances, OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances, OverloadedStrings, TemplateHaskell #-}
 
 module Tests where
 
@@ -14,68 +14,16 @@ import Data.IP
 import Data.IP.RouteTable.Internal
 import Data.List (sort, nub)
 import Prelude hiding (lookup)
-import Test.Framework (defaultMain, testGroup, Test)
 import Test.Framework.Providers.HUnit
 import Test.Framework.Providers.QuickCheck2
-import Test.HUnit hiding (Test)
+import Test.Framework.TH
+import Test.HUnit
 import Test.QuickCheck
 
-tests :: [Test]
-tests = [ testGroup "Property Test" [
-               testProperty "Sort IPv4" prop_sort_ipv4
-             , testProperty "Sort IPv6" prop_sort_ipv6
-             , testProperty "FromTo IPv4" prop_fromto_ipv4
-             , testProperty "FromTo IPv6" prop_fromto_ipv6
-             , testProperty "Insert lookup IPv4" prop_insert_lookup_ipv4
-             , testProperty "Insert lookup IPv6" prop_insert_lookup_ipv6
-             , testProperty "Search IPv4" prop_search_ipv4
-             , testProperty "Search IPv6" prop_search_ipv6
-             , testProperty "Ord IPv4" prop_ord_ipv4
-             , testProperty "Ord IPv6" prop_ord_ipv6
-             , testProperty "Delete IPv4" prop_delete_ipv4
-             , testProperty "Delete IPv6" prop_delete_ipv6
-             , testProperty "Insert delete IPv4" prop_insert_delete_ipv4
-             , testProperty "Insert delete IPv6" prop_insert_delete_ipv6
-             , testProperty "Delete insert IPv4" prop_delete_insert_ipv4
-             , testProperty "Delete insert IPv6" prop_delete_insert_ipv6
-             , testProperty "Delete lookup IPv4" prop_delete_lookup_ipv4
-             , testProperty "Delete lookup IPv6" prop_delete_lookup_ipv6
-             , testProperty "Convert fromto [Int] IPv4" prop_convert_ipv4
-             , testProperty "Convert fromto [Int] IPv6" prop_convert_ipv6
-             ]
-        , testGroup "Test case for routing table" [
-               testCase "fromto IPv4" test_fromto_ipv4
-             , testCase "lookup IPv4" test_lookup_ipv4
-             , testCase "lookup2 IPv4" test_lookup_ipv4_2
-             , testCase "findMatch IPv4" test_findMatch_ipv4
-             ]
-        , testGroup "Test case for IP" [
-               testCase "toIPv4" test_toIPv4
-             , testCase "toIPv6" test_toIPv6
-             , testCase "read IP" test_read_IP
-             , testCase "read IP 2" test_read_IP_2
-             , testCase "read IPv4" test_read_IPv4
-             , testCase "read IPv6" test_read_IPv6
-             , testCase "read IPv6 2" test_read_IPv6_2
-             , testCase "read IP range" test_read_IP_range
-             , testCase "read IP range 2" test_read_IP_range_2
-             , testCase "read IPv6 range" test_read_IPv6_range
-             , testCase "read IPv6 range" test_read_IPv6_range
-             , testCase "makeAddrRange IPv4" test_makeAddrRange_IPv4
-             , testCase "makeAddrRange IPv6" test_makeAddrRange_IPv6
-             , testCase "contains IPv4" test_contains_IPv4
-             , testCase "contains IPv4 2" test_contains_IPv4_2
-             , testCase "contains IPv6" test_contains_IPv6
-             , testCase "contains IPv6 2" test_contains_IPv6_2
-             , testCase "isMatchedTo IPv4" test_isMatchedTo_IPv4
-             , testCase "isMatchedTo IPv4 2" test_isMatchedTo_IPv4_2
-             , testCase "isMatchedTo IPv6" test_isMatchedTo_IPv6
-             , testCase "isMatchedTo IPv6 2" test_isMatchedTo_IPv6_2
-             ]
-        ]
+----------------------------------------------------------------
 
 main :: IO ()
-main = defaultMain tests
+main = $(defaultMainGenerator)
 
 ----------------------------------------------------------------
 --
@@ -254,76 +202,76 @@ prop_convert_ipv6 ip = True ==> (toIPv6 . fromIPv6) ip == ip
 
 ----------------------------------------------------------------
 
-test_toIPv4 :: Assertion
-test_toIPv4 = show (toIPv4 [192,0,2,1]) @?= "192.0.2.1"
+case_toIPv4 :: Assertion
+case_toIPv4 = show (toIPv4 [192,0,2,1]) @?= "192.0.2.1"
 
-test_toIPv6 :: Assertion
-test_toIPv6 = show (toIPv6 [0x2001,0xDB8,0,0,0,0,0,1]) @?= "2001:db8:00:00:00:00:00:01"
+case_toIPv6 :: Assertion
+case_toIPv6 = show (toIPv6 [0x2001,0xDB8,0,0,0,0,0,1]) @?= "2001:db8:00:00:00:00:00:01"
 
-test_read_IP :: Assertion
-test_read_IP = (read "192.0.2.1" :: IP) @?= IPv4 (read "192.0.2.1" :: IPv4)
+case_read_IP :: Assertion
+case_read_IP = (read "192.0.2.1" :: IP) @?= IPv4 (read "192.0.2.1" :: IPv4)
 
-test_read_IP_2 :: Assertion
-test_read_IP_2 = (read "2001:db8:00:00:00:00:00:01" :: IP) @?= IPv6 (read "2001:db8:00:00:00:00:00:01" :: IPv6)
+case_read_IP_2 :: Assertion
+case_read_IP_2 = (read "2001:db8:00:00:00:00:00:01" :: IP) @?= IPv6 (read "2001:db8:00:00:00:00:00:01" :: IPv6)
 
-test_read_IPv4 :: Assertion
-test_read_IPv4 = show (read "192.0.2.1" :: IPv4) @?= "192.0.2.1"
+case_read_IPv4 :: Assertion
+case_read_IPv4 = show (read "192.0.2.1" :: IPv4) @?= "192.0.2.1"
 
-test_read_IPv6 :: Assertion
-test_read_IPv6 = show (read "2001:db8:00:00:00:00:00:01" :: IPv6) @?= "2001:db8:00:00:00:00:00:01"
+case_read_IPv6 :: Assertion
+case_read_IPv6 = show (read "2001:db8:00:00:00:00:00:01" :: IPv6) @?= "2001:db8:00:00:00:00:00:01"
 
-test_read_IPv6_2 :: Assertion
-test_read_IPv6_2 = show (read "2001:240:11e:c00::101" :: IPv6) @?= "2001:240:11e:c00:00:00:00:101"
+case_read_IPv6_2 :: Assertion
+case_read_IPv6_2 = show (read "2001:240:11e:c00::101" :: IPv6) @?= "2001:240:11e:c00:00:00:00:101"
 
-test_read_IP_range :: Assertion
-test_read_IP_range = (read "192.0.2.1/24" :: IPRange)
+case_read_IP_range :: Assertion
+case_read_IP_range = (read "192.0.2.1/24" :: IPRange)
                  @?= IPv4Range (read "192.0.2.0/24" :: AddrRange IPv4)
 
-test_read_IP_range_2 :: Assertion
-test_read_IP_range_2 = (read "2001:db8:00:00:00:00:00:01/48" :: IPRange)
+case_read_IP_range_2 :: Assertion
+case_read_IP_range_2 = (read "2001:db8:00:00:00:00:00:01/48" :: IPRange)
                    @?= IPv6Range (read "2001:db8:00:00:00:00:00:01/48" :: AddrRange IPv6)
 
-test_read_IPv4_range :: Assertion
-test_read_IPv4_range = show (read "192.0.2.1/24" :: AddrRange IPv4) @?= "192.0.2.0/24"
+case_read_IPv4_range :: Assertion
+case_read_IPv4_range = show (read "192.0.2.1/24" :: AddrRange IPv4) @?= "192.0.2.0/24"
 
-test_read_IPv6_range :: Assertion
-test_read_IPv6_range = show (read "2001:db8:00:00:00:00:00:01/48" :: AddrRange IPv6) @?= "2001:db8:00:00:00:00:00:00/48"
-
-----------------------------------------------------------------
-
-test_makeAddrRange_IPv4 :: Assertion
-test_makeAddrRange_IPv4 = show (makeAddrRange (toIPv4 [127,0,2,1]) 8) @?= "127.0.0.0/8"
-
-test_makeAddrRange_IPv6 :: Assertion
-test_makeAddrRange_IPv6 = show (makeAddrRange (toIPv6 [0x2001,0xDB8,0,0,0,0,0,1]) 8) @?= "2000:00:00:00:00:00:00:00/8"
+case_read_IPv6_range :: Assertion
+case_read_IPv6_range = show (read "2001:db8:00:00:00:00:00:01/48" :: AddrRange IPv6) @?= "2001:db8:00:00:00:00:00:00/48"
 
 ----------------------------------------------------------------
 
-test_contains_IPv4 :: Assertion
-test_contains_IPv4 = makeAddrRange (toIPv4 [127,0,2,1]) 8 >:> makeAddrRange (toIPv4 [127,0,2,1]) 24 @?= True
+case_makeAddrRange_IPv4 :: Assertion
+case_makeAddrRange_IPv4 = show (makeAddrRange (toIPv4 [127,0,2,1]) 8) @?= "127.0.0.0/8"
 
-test_contains_IPv4_2 :: Assertion
-test_contains_IPv4_2 = makeAddrRange (toIPv4 [127,0,2,1]) 24 >:> makeAddrRange (toIPv4 [127,0,2,1]) 8 @?= False
-
-test_contains_IPv6 :: Assertion
-test_contains_IPv6 = makeAddrRange (toIPv6 [0x2001,0xDB8,0,0,0,0,0,1]) 16 >:> makeAddrRange (toIPv6 [0x2001,0xDB8,0,0,0,0,0,1]) 32 @?= True
-
-test_contains_IPv6_2 :: Assertion
-test_contains_IPv6_2 = makeAddrRange (toIPv6 [0x2001,0xDB8,0,0,0,0,0,1]) 32 >:> makeAddrRange (toIPv6 [0x2001,0xDB8,0,0,0,0,0,1]) 16 @?= False
+case_makeAddrRange_IPv6 :: Assertion
+case_makeAddrRange_IPv6 = show (makeAddrRange (toIPv6 [0x2001,0xDB8,0,0,0,0,0,1]) 8) @?= "2000:00:00:00:00:00:00:00/8"
 
 ----------------------------------------------------------------
 
-test_isMatchedTo_IPv4 :: Assertion
-test_isMatchedTo_IPv4 = toIPv4 [127,0,2,1] `isMatchedTo` makeAddrRange (toIPv4 [127,0,2,1]) 24 @?= True
+case_contains_IPv4 :: Assertion
+case_contains_IPv4 = makeAddrRange (toIPv4 [127,0,2,1]) 8 >:> makeAddrRange (toIPv4 [127,0,2,1]) 24 @?= True
 
-test_isMatchedTo_IPv4_2 :: Assertion
-test_isMatchedTo_IPv4_2 = toIPv4 [127,0,2,0] `isMatchedTo` makeAddrRange (toIPv4 [127,0,2,1]) 32 @?= False
+case_contains_IPv4_2 :: Assertion
+case_contains_IPv4_2 = makeAddrRange (toIPv4 [127,0,2,1]) 24 >:> makeAddrRange (toIPv4 [127,0,2,1]) 8 @?= False
 
-test_isMatchedTo_IPv6 :: Assertion
-test_isMatchedTo_IPv6 = toIPv6 [0x2001,0xDB8,0,0,0,0,0,1] `isMatchedTo` makeAddrRange (toIPv6 [0x2001,0xDB8,0,0,0,0,0,1]) 32 @?= True
+case_contains_IPv6 :: Assertion
+case_contains_IPv6 = makeAddrRange (toIPv6 [0x2001,0xDB8,0,0,0,0,0,1]) 16 >:> makeAddrRange (toIPv6 [0x2001,0xDB8,0,0,0,0,0,1]) 32 @?= True
 
-test_isMatchedTo_IPv6_2 :: Assertion
-test_isMatchedTo_IPv6_2 = toIPv6 [0x2001,0xDB8,0,0,0,0,0,0] `isMatchedTo` makeAddrRange (toIPv6 [0x2001,0xDB8,0,0,0,0,0,1]) 128 @?= False
+case_contains_IPv6_2 :: Assertion
+case_contains_IPv6_2 = makeAddrRange (toIPv6 [0x2001,0xDB8,0,0,0,0,0,1]) 32 >:> makeAddrRange (toIPv6 [0x2001,0xDB8,0,0,0,0,0,1]) 16 @?= False
+
+----------------------------------------------------------------
+
+case_isMatchedTo_IPv4 :: Assertion
+case_isMatchedTo_IPv4 = toIPv4 [127,0,2,1] `isMatchedTo` makeAddrRange (toIPv4 [127,0,2,1]) 24 @?= True
+
+case_isMatchedTo_IPv4_2 :: Assertion
+case_isMatchedTo_IPv4_2 = toIPv4 [127,0,2,0] `isMatchedTo` makeAddrRange (toIPv4 [127,0,2,1]) 32 @?= False
+
+case_isMatchedTo_IPv6 :: Assertion
+case_isMatchedTo_IPv6 = toIPv6 [0x2001,0xDB8,0,0,0,0,0,1] `isMatchedTo` makeAddrRange (toIPv6 [0x2001,0xDB8,0,0,0,0,0,1]) 32 @?= True
+
+case_isMatchedTo_IPv6_2 :: Assertion
+case_isMatchedTo_IPv6_2 = toIPv6 [0x2001,0xDB8,0,0,0,0,0,0] `isMatchedTo` makeAddrRange (toIPv6 [0x2001,0xDB8,0,0,0,0,0,1]) 128 @?= False
 
 ----------------------------------------------------------------
 
@@ -336,13 +284,13 @@ ipv4_list = [
   , "133.5.23.0/24"
   ]
 
-test_fromto_ipv4 :: Assertion
-test_fromto_ipv4 = (sort . toList . fromList $ pairs) @?= pairs
+case_fromto_ipv4 :: Assertion
+case_fromto_ipv4 = (sort . toList . fromList $ pairs) @?= pairs
   where
     pairs = sort $ zip ipv4_list ipv4_list
 
-test_lookup_ipv4 :: Assertion
-test_lookup_ipv4 = do
+case_lookup_ipv4 :: Assertion
+case_lookup_ipv4 = do
     let rt = fromList pairs
     lookup "127.0.0.1" rt @?= Just "0.0.0.0/0"
     lookup "133.3.0.1" rt @?= Just "0.0.0.0/0"
@@ -353,8 +301,8 @@ test_lookup_ipv4 = do
   where
     pairs = zip ipv4_list ipv4_list
 
-test_lookup_ipv4_2 :: Assertion
-test_lookup_ipv4_2 = do
+case_lookup_ipv4_2 :: Assertion
+case_lookup_ipv4_2 = do
     let rt = fromList pairs
     lookup "127.0.0.1" rt @?= Nothing
     lookup "133.3.0.1" rt @?= Nothing
@@ -366,8 +314,8 @@ test_lookup_ipv4_2 = do
     ipv4_list' = tail ipv4_list
     pairs = zip ipv4_list' ipv4_list'
 
-test_findMatch_ipv4 :: Assertion
-test_findMatch_ipv4 = do
+case_findMatch_ipv4 :: Assertion
+case_findMatch_ipv4 = do
     let rt = fromList pairs
     findMatch "127.0.0.1" rt @?= []
     findMatch "133.5.23.0/23" rt @?= [("133.5.23.0/24","133.5.23.0/24")]
