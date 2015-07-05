c1554e622d897df14a701ab584cd286150561bd5
Lana Black <lanablack@amok.cc>
2015-07-05 23:22:18 +0000

    Expose IPv4, IPv6, and AddrRange constructors through Data.IP.Internal

---
+++ Data/IP/Internal.hs
@@ -0,0 +1,7 @@
+module Data.IP.Internal ( IPv4(..)
+                        , IPv6(..)
+                        , AddrRange(..)
+                        ) where
+
+import Data.IP.Addr
+import Data.IP.Range
