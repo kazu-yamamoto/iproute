f51cfc0a202cf4f5ab1c0cd859c0ee42abd19eef
Kazu Yamamoto <kazu@iij.ad.jp>
2018-04-19 10:00:27 +0900

    updating doc.

--- Data/IP.hs
+++ Data/IP.hs
@@ -2,18 +2,21 @@
   Data structures to express IPv4, IPv6 and IP range.
 -}
 module Data.IP (
-  -- * Documentation
-  -- ** IP data
+  -- * IP data
     IP (..)
+  -- ** IPv4
   , IPv4, toIPv4, fromIPv4, fromHostAddress, toHostAddress
+  -- ** IPv6
   , IPv6, toIPv6, toIPv6b, fromIPv6, fromIPv6b, fromHostAddress6, toHostAddress6
-  -- ** IP range data
+  -- ** Converters
+  , ipv4ToIPv6
+  -- * IP range data
   , IPRange (..)
   , AddrRange (addr, mask, mlen)
-  -- ** Address class
+  -- * Address class
   , Addr (..)
   , makeAddrRange, (>:>), isMatchedTo, addrRangePair
-  , ipv4RangeToIPv6, ipv4ToIPv6
+  , ipv4RangeToIPv6
   ) where
 
 import Data.IP.Addr
