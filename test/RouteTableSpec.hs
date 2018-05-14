aecbd3a58fc9edf76c97d94e82b9bdbf2c4d70ff
Lana Black <lanablack@amok.cc>
2018-05-14 16:57:40 +0000

    Add tests for folds and iprtable monoid instance.

--- test/RouteTableSpec.hs
+++ test/RouteTableSpec.hs
@@ -5,12 +5,16 @@
 module RouteTableSpec where
 
 #if __GLASGOW_HASKELL__ < 709
-import Control.Applicative
+import Control.Applicative hiding (empty)
 #endif
 import Control.Monad
+import Data.Function (on)
 import Data.IP
 import Data.IP.RouteTable.Internal
+import qualified Data.Foldable as Foldable
 import Data.List (sort, nub)
+import qualified Data.List as List
+import Data.Monoid ((<>))
 import Test.Hspec
 import Test.Hspec.QuickCheck (prop)
 import Test.QuickCheck
@@ -59,6 +63,11 @@ spec = do
             (fromto_ip :: [AddrRange IPv4] -> Bool)
         prop "expands as sorted"
             (fromto_ip :: [AddrRange IPv6] -> Bool)
+    describe "folds" $ do
+        prop "foldl" prop_foldl
+        prop "foldr" prop_foldr
+    describe "monoid" $ do
+        prop "monoid instance" prop_monoid
 
 sort_ip :: (Routable a, Ord a) => [AddrRange a] -> Bool
 sort_ip xs = fromList (zip xs xs) == fromList (zip xs' xs')
@@ -82,3 +91,28 @@ ordered (Node k _ _ l r) = ordered' k l && ordered' k r
   where
     ordered' _ Nil = True
     ordered' k1 (Node k2 _ _ _ _) = k1 >:> k2
+
+-- Foldl and foldr properties are adapted from Data.Map tests
+prop_foldl :: Int -> [(AddrRange IPv4, Int)] -> Property
+prop_foldl n ys = length ys > 0 ==>
+  let xs = List.nubBy ((==) `on` fst) ys
+      m  = fromList xs
+  in  Foldable.foldl (+) n m == List.foldr (+) n (List.map snd xs) &&
+      Foldable.foldl (flip (:)) [] m == reverse (List.map snd (List.sort xs)) &&
+      foldlWithKey (\b _ a -> a + b) n m == List.foldr (+) n (List.map snd xs)
+
+prop_foldr :: Int -> [(AddrRange IPv4, Int)] -> Property
+prop_foldr n ys = length ys > 0 ==>
+  let xs = List.nubBy ((==) `on` fst) ys
+      m  = fromList xs
+  in  Foldable.foldr (+) n m == List.foldr (+) n (List.map snd xs) &&
+      Foldable.foldr (:) [] m == List.map snd (List.sortBy (compare `on` fst) xs) &&
+      foldrWithKey (\_ a b -> a + b) n m == List.foldr (+) n (List.map snd xs)
+
+prop_monoid :: [(AddrRange IPv4, ())] -> [(AddrRange IPv4, ())] -> Property
+prop_monoid xs ys = length xs > 0 && length ys > 0 ==>
+  let xm = fromList xs
+      ym = fromList ys
+  in empty <> xm == xm
+  && ym <> empty == ym
+  && xm <> ym == fromList (xs ++ ys)
