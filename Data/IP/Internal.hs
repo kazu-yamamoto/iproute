9e91f079dc290dda1ec7113eada704e9bd8a9513
Kazu Yamamoto <kazu@iij.ad.jp>
2018-10-17 12:01:43 +0900

    exporting IPv4Addr and IPv6Addr from Internal.

--- Data/IP/Internal.hs
+++ Data/IP/Internal.hs
@@ -1,6 +1,8 @@
 module Data.IP.Internal ( IPv4(..)
                         , IPv6(..)
                         , AddrRange(..)
+                        , IPv4Addr
+                        , IPv6Addr
                         ) where
 
 import Data.IP.Addr
